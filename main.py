9f160e454adf045aeaf71cbaef45c09e539a2964
faretwe <faretwe4747@gmail.com>
2025-08-01 01:35:38 -0700

    Add initial Flask backend files

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+# DON'T CHANGE THIS !!!
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+from src.models.user import db
+from src.routes.user import user_bp
+from src.routes.comment import comment_bp
+
+app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
+app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
+
+# Enable CORS for all routes
+CORS(app)
+
+app.register_blueprint(user_bp, url_prefix='/api')
+app.register_blueprint(comment_bp, url_prefix='/api')
+
+# uncomment if you need to use database
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
+
+if __name__ == '__main__':
+    with app.app_context():
+        os.makedirs(os.path.join(os.path.dirname(__file__), 'database'), exist_ok=True)
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
